80a25b0e3826fb9c67f3b6601fa00e335a162f88
Daria Nikolaenko <nikolaenko.darya@icloud.com>
2016-12-05 18:57:26 +0300

    DAO KPI department has been created

---
+++ local/modules/miet.kpi/include.php
@@ -0,0 +1,7 @@
+<?
+IncludeModuleLangFile(__FILE__);
+Bitrix\Main\Loader::includeModule("iblock");
+require_once "lib/kpidepartment.php";
+require_once "lib/kpiemployee.php";
+require_once "lib/kpimanager.php";
+?>
---
+++ local/modules/miet.kpi/lang/ru/install/index.php
@@ -0,0 +1,6 @@
+<?
+$MESS["miet.kpi_MODULE_NAME"] = "МИЭТ. KPI";
+$MESS["miet.kpi_MODULE_DESC"] = "Оценка KPI сотрудников и подразделений";
+$MESS["miet.kpi_PARTNER_NAME"] = "МИЭТ";
+$MESS["miet.kpi_PARTNER_URI"] = "http://miet.ru/";
+?>
